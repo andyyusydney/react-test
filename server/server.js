2bb584c6c77b2dee4446785780a2204605d89098
Andy Yu <yyujjiang@hotmail.com>
2016-03-05 15:32:36 +1100

    -- created server api to handle pagination of articles list request.

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var config = require('../webpack.config.js');
 var webpack = require('webpack');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
-
+var fs = require("fs")
 var app = express();
 
 var compiler = webpack(config);
@@ -13,6 +13,34 @@ app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.
 app.use(webpackHotMiddleware(compiler));
 
 app.use(express.static('./dist'));
+
+
+
+function readJsonFileSync(filepath, encoding){
+
+    if (typeof (encoding) == 'undefined'){
+        encoding = 'utf8';
+    }
+    var file = fs.readFileSync(filepath, encoding);
+    return JSON.parse(file);
+}
+
+function getConfig(file){
+    var filepath = __dirname + '/' + file;
+    return readJsonFileSync(filepath);
+}
+
+var pageSize = 3;
+app.get('/articles/:page', function (req, res) {
+	var page = req.params.page || 1;
+	console.log("page=", page);
+	//console.log(getConfig('articlesList.json'))
+	var articlesList = getConfig('articlesList.json');
+	
+	var currentPageList = articlesList.slice(pageSize * (page-1), pageSize * page);
+	console.log("currentPageList=", currentPageList);
+    res.send(JSON.stringify(currentPageList, null, 3));
+});
 
 app.use('/', function (req, res) {
     res.sendFile(path.resolve('client/index.html'));
